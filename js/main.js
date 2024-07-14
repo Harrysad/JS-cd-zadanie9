b3c2ced7f89c8a8d0c2a620111785bda44e717ab
sdebowski <s_debowski@wp.pl>
2024-07-14 15:50:13 +0200

    Wykonanie zadania

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* function iloczyn(num1, num2, num3) {
+    let result = num1 * num2 * num3;
+    return result;
+} */
+
+const iloczyn = (num1, num2, num3) => {
+    let result = num1 * num2 * num3;
+
+    return result;
+}
+
+let wynikIloczynu = iloczyn(2, 3, 4);
+
+console.log(wynikIloczynu);
